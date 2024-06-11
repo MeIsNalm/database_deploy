db948abd763be02c26dca4c293b7d75bfbbf40c0
MeIsNalm <nuno.alexandre30@gmail.com>
2024-06-11 20:06:38 +0100

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS contacts;
-CREATE TABLE contacts (
+DROP TABLE IF EXISTS contacts_NM;
+CREATE TABLE contacts_NM (
 id SERIAL PRIMARY KEY, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, email VARCHAR(384) NOT NULL UNIQUE
 );
-INSERT INTO contacts (first_name, last_name, email)
+INSERT INTO contacts_NM (first_name, last_name, email)
 VALUES
 ('John', 'Doe', 'john.doe@example.com'), ('Jane', 'Smith', 'jane.smith@example.com'), ('Bob', 'Johnson', 'bob.johnson@example.com');
